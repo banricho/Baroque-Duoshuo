777160d428c7f6a04b803d0589a1fecd2bce4239
banrikun <i@banri.me>
2015-01-19 13:04:00 +0800

    优化体验

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <title><?php if ( $this->is('single') || $this->is('page') || $this->is('category') || $this->is('tag') ) { $this->archiveTitle('','',''); } else { $this->options->title(); } ?></title>
 <link rel="stylesheet" type="text/css" href="<?php $this->options->themeUrl('style.css'); ?>" />
 <?php $this->header("generator=&template="); ?>
